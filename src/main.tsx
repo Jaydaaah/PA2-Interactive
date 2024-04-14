405138a2ce12907579f92008688235c5ab66f67c
Jaydaaah <sv.jdrapha@gmail.com>
2024-04-14 18:38:28 +0800

    4th deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { HashRouter, Route, Routes } from 'react-router-dom'
 import Admin from './Admin.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename='/PA2-Interactive'>
-      <Routes>
-        <Route Component={App} path='/'/>
-        <Route Component={Admin} path='/admin'/>
-      </Routes>
-    </HashRouter>
+    <App/>
   </React.StrictMode>,
 )
